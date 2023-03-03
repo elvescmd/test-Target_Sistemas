e1acaa9ff136d77da308a3e72a1a68d98b892a63
Elves S <elves.s@outlook.com>
2023-03-03 15:12:44 -0300

    Exercicio de soma

---
+++ Exercicio-1/Soma.js
@@ -0,0 +1,13 @@
+//Para ler a lógica de forma clara, refiz o código usando nodejs para uma melhor compreenção e explicação.
+
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+while (K < INDICE) {
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+console.log(SOMA);
+ //resposta = 91
